6eec1d0d0a5a6c51945c0b9e2ee3a19a7a1b4f39
ruben2daw <root@dwes>
2016-09-20 16:57:42 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+var a = 3;
+var b = "asdf";
+var r1 = a * b;
+console.log(r1);
+
+var c;
+var r3 = a + c;
+console.log(r3);
+
+var d = "12";
+console.log(a * d);
+console.log(a + d);
+console.log(a + +d);
+
+//UNDEFINED Y NULL
+var value;//Value = undefined
+console.log(value);
+value = null;
+console.log(typeof value);
